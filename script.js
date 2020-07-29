cbedd6dc8f4d94ac1cc038eebe44520ae3234d1f
Karman Badhesha <karman.badhesha@edu.sait.ca>
2020-07-29 12:22:24 -0600

    add capstone page

--- script.js
+++ script.js
@@ -1,39 +1,52 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-        // Transition effect for navbar and back-to-top icon
-        $(window).scroll(function() {
-          // checks if window is scrolled more than 500px, adds/removes solid class
-          if($(this).scrollTop() > 550) { 
-              $('.navbar').addClass('solid');
-              $('.back-to-top').addClass('visible'); 
-          } else {
-              $('.navbar').removeClass('solid');
-              $('.back-to-top').removeClass('visible');  
-          }
+    // Transition effect for navbar and back-to-top icon
+    $(window).scroll(function () {
+        // checks if window is scrolled more than 500px, adds/removes solid class
+        if ($(this).scrollTop() > 550) {
+            $('.navbar').addClass('solid');
+            $('.back-to-top').addClass('visible');
+        } else {
+            $('.navbar').removeClass('solid');
+            $('.back-to-top').removeClass('visible');
+        }
 
-        });
+    });
 
 
-        // Scrolling effect for Arrow icons
-        $("#scrollIcon").click(function(e) {
-            e.preventDefault();
-            $.scrollTo($("#about"), 1000);
-        });
-        $("#nav-about").click(function(e) {
-            e.preventDefault();
-            $.scrollTo($("#about"), 1000);
-        });
-        $("#nav-portfolio").click(function(e) {
-            e.preventDefault();
-            $.scrollTo($("#portfolio"), 1000);
-        });
-        $("#nav-contact").click(function(e) {
-            e.preventDefault();
-            $.scrollTo($("#contact"), 1000);
-        });
-        $(".navbar-brand").click(function(e) {
-            e.preventDefault();
-            $.scrollTo(0, 1000);
-        });
-          
-      });
+    // Scrolling effect for Arrow icons
+    $("#scrollIcon").click(function (e) {
+        e.preventDefault();
+        $.scrollTo($("#about"), 1000);
+    });
+    $("#nav-about").click(function (e) {
+        e.preventDefault();
+        $.scrollTo($("#about"), 1000);
+    });
+    $("#nav-portfolio").click(function (e) {
+        e.preventDefault();
+        $.scrollTo($("#portfolio"), 1000);
+    });
+    $("#nav-contact").click(function (e) {
+        e.preventDefault();
+        $.scrollTo($("#contact"), 1000);
+    });
+    $(".navbar-brand").click(function (e) {
+        e.preventDefault();
+        $.scrollTo(0, 1000);
+    });
+
+});
+
+function myFunction(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+} 
